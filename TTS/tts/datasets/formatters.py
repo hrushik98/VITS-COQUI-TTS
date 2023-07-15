c8e9f515fca6a00144966460f55f4245cdc41661
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-07-15 17:48:41 +0530

    fixed " error

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -158,8 +158,13 @@ def ljspeech(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     with open(txt_file, "r", encoding="utf-8") as ttf:
         for line in ttf:
             cols = line.split("|")
-            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
-            text = cols[2]
+            if cols[0].startswith('"'):
+
+              wav_file = os.path.join(root_path, "wavs", cols[0][1:] + ".wav")
+            else:
+              wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+
+            text = cols[1]
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
     return items
 
